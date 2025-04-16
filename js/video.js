8c96485bbeab76c9008186d0ca5f984b4989aa5f
elizabeth-nestle <elnestle@umich.edu>
2025-04-15 22:02:13 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,101 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	
+	// vid.load();
+	
+	// vid.play();
+
+	
+
+});
+
+
+let vid = document.getElementById("player1");
+
+// vid.addEventListener("load", function() {
+vid.autoplay = false;
+vid.loop = false;
+// });
+
+let playB = document.getElementById("play");
+let currVolume = document.getElementById("slider").value;
+
+playB.addEventListener("click", function () {
+	vid.play();
+	vid.volume = currVolume / 200;
+	
+});
+
+let pauseB = document.getElementById("pause");
+
+pauseB.addEventListener("click", function () {
+	vid.pause();
+});
+
+let speedUp = document.getElementById("faster");
+
+speedUp.addEventListener("click", function () {
+	vid.playbackRate += 0.1; 
+	console.log("Playback rate is", vid.playbackRate)
+});
+
+let speedDown = document.getElementById("slower");
+
+speedDown.addEventListener("click", function () {
+	vid.playbackRate -= 0.1; 
+	console.log("Playback rate is", vid.playbackRate)
+});
+
+let vol = document.getElementById("slider")
+
+
+
+// vol.getEventListener("click", function () {
+// 	currVolume = vol.value;
+// });
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
+let shutUp = document.getElementById("mute")
+
+shutUp.addEventListener("click", function(){
+	if (vid.muted == true){
+		vid.muted = false
+	}
+	else if (vid.muted == false){
+		vid.muted = true
+	}})
+
+let theZoomies = document.getElementById("skip")
+
+theZoomies.addEventListener("click", function(){
+	if (vid.currentTime < 57.453968){
+		vid.currentTime = vid.currentTime + 10
+	}
+	else {vid.currentTime = 0.00}
+	console.log("Current location is", vid.currentTime)
+})
+
+let oldBoi = document.getElementById("vintage")
+
+oldBoi.addEventListener("click", function(){
+	var element = video.querySelector('vintage');
+	element.className += ' oldSchool';
+})
+
+
+
+
+// This code is from W3Schools //
+var slider = document.getElementById("slider");
+var output = document.getElementById("volume");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+} 
+// End code from W3Schools //
+
---
+++ js/video_old.js
@@ -0,0 +1,29 @@
+var video;
+
+window.addEventListener("load", function () {
+	console.log("Good job opening the window")
+	let vid = window.getElementById("player1")
+	vid.loop = false;
+	vid.autoplay = false;
+	vid.play();
+
+
+});
+
+// window.getElementById("player1").addEventListener("load", function() {
+// 	vid.loop = false
+// 	vid.autoplay = false
+// });
+
+window.getElementById("play").addEventListener("click", function () {
+
+})
+
+
+
+
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
